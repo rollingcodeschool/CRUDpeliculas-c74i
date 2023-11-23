80e914379eebd72c2c9eb257b5efbe35fa6b36a2
Emilse Arias <emilsearias@gmail.com>
2023-11-23 13:33:28 -0300

    creamos la funcion crearPelicula

--- js/administrador.js
+++ js/administrador.js
@@ -1,4 +1,19 @@
 import Pelicula from "./classPelicula.js";
 
-const peliNueva = new Pelicula(1, 'batman', 'djhfskdjf', 'ashdgahjs', 'Accion');
-console.log(peliNueva);
+//variables
+const formularioAdminPelis = document.querySelector('#formularioAdministrarPelicula');
+
+//funciones
+const crearPelicula = () =>{
+    //tomar toda la info del formulario
+
+
+    
+    //crear el objeto
+    const peliNueva = new Pelicula(1, 'batman', 'djhfskdjf', 'ashdgahjs', 'Accion');
+    //guardar lista de peliculas en localstorage
+}
+
+
+//resto de la logica
+formularioAdminPelis.addEventListener('submit', crearPelicula)
