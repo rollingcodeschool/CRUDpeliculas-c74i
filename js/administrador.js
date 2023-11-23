e7a61ef9e8740f832400c406fe0b1364f53ad7c2
Emilse Arias <emilsearias@gmail.com>
2023-11-23 11:23:18 -0300

    agregamos la clase pelicula y la utilizamos en administrador.js

---
+++ js/administrador.js
@@ -0,0 +1,4 @@
+import Pelicula from "./classPelicula.js";
+
+const peliNueva = new Pelicula(1, 'batman', 'djhfskdjf', 'ashdgahjs', 'Accion');
+console.log(peliNueva);
---
+++ js/classPelicula.js
@@ -0,0 +1,9 @@
+export default class Pelicula{
+    constructor(codigo, titulo, descripcion, imagen, genero){
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+        this.genero = genero;
+    }
+}
