1f79701e6f8c5bc5498ae95430b53f112ada1b3c
Emilse Arias <emilsearias@gmail.com>
2023-11-28 09:43:36 -0300

    agregamos la ventana modal a nuestro adminPeliculas

--- js/administrador.js
+++ js/administrador.js
@@ -2,6 +2,15 @@ import Pelicula from "./classPelicula.js";
 
 //variables
 const formularioAdminPelis = document.querySelector('#formularioAdministrarPelicula');
+const codigo = document.getElementById("codigo"),
+  titulo = document.getElementById("titulo"),
+  descripcion = document.getElementById("descripcion"),
+  imagen = document.getElementById("imagen"),
+  genero = document.getElementById("genero"),
+  modalPelicula = new bootstrap.Modal(document.getElementById('modalPelicula'));
+
+  modalPelicula.show();
+
 
 //funciones
 const crearPelicula = () =>{
