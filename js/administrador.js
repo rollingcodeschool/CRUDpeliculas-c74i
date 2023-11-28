7f58da17222410e5d25f8b5e795cbd61e0a73c94
Emilse Arias <emilsearias@gmail.com>
2023-11-28 09:48:02 -0300

    llamamos la ventana modal desde la funcion abrirModalPelicula

--- js/administrador.js
+++ js/administrador.js
@@ -7,22 +7,23 @@ const codigo = document.getElementById("codigo"),
   descripcion = document.getElementById("descripcion"),
   imagen = document.getElementById("imagen"),
   genero = document.getElementById("genero"),
-  modalPelicula = new bootstrap.Modal(document.getElementById('modalPelicula'));
-
-  modalPelicula.show();
+  modalPelicula = new bootstrap.Modal(document.getElementById('modalPelicula')),
+  btnAgregar = document.getElementById('btnAgregar');
 
 
 //funciones
 const crearPelicula = () =>{
     //tomar toda la info del formulario
-
-
-    
     //crear el objeto
     const peliNueva = new Pelicula(1, 'batman', 'djhfskdjf', 'ashdgahjs', 'Accion');
     //guardar lista de peliculas en localstorage
 }
 
+const abrirModalPelicula = ()=>{
+    modalPelicula.show();
+}
+
 
 //resto de la logica
 formularioAdminPelis.addEventListener('submit', crearPelicula)
+btnAgregar.addEventListener('click', abrirModalPelicula)
